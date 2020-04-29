fc1a8c4c9a1906e044c319f91e989d8d27df0b29
AssafR <assaf@razon-family.com>
2020-04-29 16:12:40 +0300

    First working version

---
+++ find_image_duplicates.py
@@ -0,0 +1,30 @@
+import pprint
+from os.path import relpath
+from pathlib import Path
+from filehash import FileHash
+import glob
+
+basedir = Path("C:\\Labs\\Data\\")  # "R:\\mp3\\שחר סגל ורועי בר נתן\\גלי צה_ל\\"
+
+
+def insert_file_to_dict(hashdict:dict,hasher, file):
+    file_hash = hasher.hash_file(file)
+    if file_hash not in hashdict:
+        hashdict[file_hash] = []
+    shortfile = relpath(file,basedir)
+    hashdict[file_hash].append(shortfile)
+
+if __name__ == "__main__":
+    files_by_hash = {}
+    md5hasher = FileHash('md5')
+    files = glob.glob(str(basedir) + '/**/*.png', recursive=True)
+    for i, file in enumerate(files):
+        # if i>100:
+        #     break;
+        insert_file_to_dict(files_by_hash,md5hasher,file)
+
+    for hash, images in files_by_hash.items():
+        if len(images)>1:
+            print(images)
+
+
